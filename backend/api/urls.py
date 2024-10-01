24047246e62412ca0d101e9634514cf46482ef2e
afnaann <afnanpk8@gmail.com>
2024-10-01 09:22:49 +0530

    Updated urls.py for better routing

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from .serializers import MyTokenObtainPairView
+from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
+from .serializers import MyTokenObtainPairView
 urlpatterns = [
     path('register/',views.register),
-    path('api/login',TokenObtainPairView.as_view(),name='token_obtain_pair'),
-    path('api/login/refresh',TokenRefreshView.as_view(),name='token_refresh')
+    
+    path('api/login',MyTokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('api/login/refresh',TokenRefreshView.as_view(),name='token_refresh'),
+    
+    path('tasks/get',views.getTasks.as_view()), # Not Using Currently
+    path('tasks/home',views.userDashboard.as_view()),
+    path('tasks/submit',views.submitTask.as_view()),
+    
+    path('admin/getusers',views.getUsers.as_view()),
+    path('admin/alltasks',views.allUserTasks.as_view())
 ]
+
