4dc2580b1c6560edbc909a09276a144e222c251c
afnaann <afnanpk8@gmail.com>
2024-10-01 09:23:42 +0530

    Updated API admin and views; adjusted .gitignore

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from .serializers import UserSerializer
+from .serializers import TaskSerializer, UserSerializer, UserTaskSerializer, UserGetSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from django.core.mail import send_mail
 from django.core.mail import EmailMessage
 from backend.settings import EMAIL_HOST_USER
 from rest_framework.decorators import permission_classes
-from rest_framework.permissions import AllowAny
-
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework.views import APIView
+from .models import Task,UserTasks
+from django.db.models import Q
+from django.contrib.auth.models import User
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -16,10 +19,6 @@ def register(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        print(request.data)
-        print(request.data['username'])
-        print(request.data['password'])
-        print(request.data['email'])
         subject = 'You Can now login to Task Management!'
         message = (
             f"Dear {request.data['username']},\n\n"
@@ -35,3 +34,49 @@ def register(request):
         send_mail(subject,message,EMAIL_HOST_USER,recipient_list,fail_silently=True)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@permission_classes([IsAuthenticated])
+class getTasks(APIView):
+    def get(self,request):
+        obj = Task.objects.all()
+        serializer = TaskSerializer(obj,many=True)
+        return Response(serializer.data)
+    
+@permission_classes([IsAuthenticated])
+class userDashboard(APIView):
+    def get(self,request):
+        usertasks = UserTasks.objects.filter(user=request.user)
+        serializer = UserTaskSerializer(usertasks,many=True)
+        return Response(serializer.data)
+    
+@permission_classes([IsAuthenticated])
+class allUserTasks(APIView):
+    def get(self,request):
+        tasks = UserTasks.objects.all()
+        serializer = UserTaskSerializer(tasks,many=True)
+        return Response(serializer.data)
+        
+@permission_classes([IsAuthenticated])
+class submitTask(APIView):
+    def post(self,request):
+        title = request.data.get('title')
+        print(title)
+        
+        task = Task.objects.filter(title=title).first()
+        print(task)
+        
+        usertask = UserTasks.objects.select_related('task','user').filter(task=task,user=request.user).first()
+        
+        if usertask:
+            print('happy')
+            usertask.completed = True
+            usertask.save()
+            return Response({'Message':'Task marked as Completed!'})
+        return Response({'Error':'Task marking error!'})
+    
+@permission_classes([IsAuthenticated])
+class getUsers(APIView):
+    def get(self,request):
+        users = User.objects.filter(is_staff=False)
+        serializer = UserGetSerializer(users, many=True)
+        return Response(serializer.data)
